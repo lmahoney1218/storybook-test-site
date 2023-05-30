874053d5bbabfb63c21b1edadbf0f871c9b48515
Laura Mahoney <landerson1218@gmail.com>
2023-05-30 10:54:44 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `storybook-test-site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
